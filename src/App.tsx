896abd42b5d2f4f446d4c3a390472b9490c35e6b
Michele Memoli <michele.memoli@gmail.com>
2026-02-07 16:38:50 +0700

    fix(app): added state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Atom, useAtomValue, useAtomSet } from "@effect-atom/atom-react";
+import { useCallback } from "react";
+
+const countAtom = Atom.make(0).pipe(Atom.keepAlive);
+
+const useIncrement = () => {
+	const set = useAtomSet(countAtom);
+	return useCallback(() => set((prev) => prev + 1), [set]);
+};
+
+const useDecrement = () => {
+	const set = useAtomSet(countAtom);
+	return useCallback(() => set((prev) => prev - 1), [set]);
+};
+
 function App() {
-	return <p>Yo</p>;
+	const count = useAtomValue(countAtom);
+	const increment = useIncrement();
+	const decrement = useDecrement();
+	return (
+		<p>
+			<span data-testid="count">{count}</span>
+			<button type="button" onClick={increment}>
+				+
+			</button>
+			<button type="button" onClick={decrement}>
+				-
+			</button>
+		</p>
+	);
 }
 
 export default App;
